745e89d7c9812719672edcec5827a94fe9873b51
Jonathan Rux <jonathan.e.rux@gmail.com>
2023-04-03 23:45:47 -0700

    Removed winget-cli and readded ability check for command and install winget and prereqs.

--- initialize.ps1
+++ initialize.ps1
@@ -20,29 +20,42 @@
 #########################
 
 function InstallApps { #Check for Winget, Install Winget if it isn't installed, and then install apps from a list.
-    Write-Output "Installing Applications"
+    Write-Output "Checking for Winget..."
+    if (Get-Command winget) {
+        Write-Output "Winget is installed.`nContinuing..."
+    } else {
+        Write-Output "Winget doesn't exist.`nInstalling Winget...`nWinget is being downloaded..."
+        $ProgressPreference='Silent'
+        Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.3.2691/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile ( New-Item -Path .\packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -Force )
+        Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile .\packages\Microsoft.VCLibs.x64.14.00.Desktop.appx
+        Write-Output "Installing winget..."
+        Add-AppxPackage .\packages\Microsoft.VCLibs.x64.14.00.Desktop.appx
+        Add-AppxPackage .\packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+    }
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+    Set-ExecutionPolicy Unrestricted
+    Write-Output "Installing Applications..."
     $Apps = @( #Define's application's names as appears in winget.
-        @{name = "7zip.7zip"}
-        @{name = "Git.Git"}
-        @{name = "Google.Chrome"}
-        @{name = "Microsoft.PowerShell"}
-        @{name = "Microsoft.PowerToys"}
-        @{name = "Microsoft.VisualStudioCode"}
-        @{name = "Microsoft.VisualStudio.2022.Community"}
-        @{name = "Microsoft.WindowsTerminal"}
-        @{name = "Notepad++.Notepad++"}
-        @{name = "Microsoft.Teams"}
-        @{name = "voidtools.Everything.Lite"}
+        "7zip.7zip",
+        "Git.Git",
+        "Google.Chrome",
+        "Microsoft.PowerShell",
+        "Microsoft.PowerToys",
+        "Microsoft.VisualStudioCode",
+        "Microsoft.VisualStudio.2022.Community",
+        "Microsoft.WindowsTerminal",
+        "Notepad++.Notepad++",
+        "Microsoft.Teams",
+        "voidtools.Everything.Lite"
     )
 
-    $Command = ".\winget-cli\winget.exe"
     foreach ($App in $Apps) {
-        $listApp = winget list --exact -q $App.name
-        if (![String]::Join("", $listApp).Contains($App.name)) {
-            Write-Output "Installing: " $App.name
-            $Command install -e -h --accept-source-agreements --accept-package-agreements --id $App.name
+        $listApp = winget list --exact -q $App
+        if ($listApp -contains $App) {
+            Write-Output "Installing: $($App)..."
+            winget install -e -h --accept-source-agreements --accept-package-agreements --id $App
         } else {
-            Write-Output "Skipping:  $($App.name) is already installed; skipping."
+            Write-Output "Skipping:  $($App) is already installed; Skipping..."
         }
     }
 }
