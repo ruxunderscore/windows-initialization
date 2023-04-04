d062fddcf6bda8025f9f26eb915d2c27e4d2817e
Jonathan Rux <jonathan.e.rux@gmail.com>
2023-04-04 01:24:25 -0700

    Added function for Appx Installs. Added function for titles to assist with readability. Created seperate function for winget prerequisites. Added Cleanup of winget installer.

--- initialize.ps1
+++ initialize.ps1
@@ -4,37 +4,75 @@
 #       Author        : Jonathan Rux            |  \   \/\/   /  |/    \  / __ |/  _ \ \/ \/ /  ___/  #
 #       Email         : jrux@email.com          |   \        /|  |   |  \/ /_/ (  <_> )     /\___ \   #
 #       Version       : 1.0                     |    \__/\  / |__|___|  /\____ |\____/ \/\_//____  >  #
-#       OS Support    : Windows 10,11           |         \/          \/      \/                 \/   #
+#       OS Support    : Windows 10,11 x64       |         \/          \/      \/                 \/   #
 #                                               |       Initialization Setup Script.                  #
 #                                               |                                                     #
 #######################################################################################################
 
 <#
 .DESCRIPTION
-        This script initializes Windows 10/11 and, then, installs initial software needed by
-        employees at <insert company here>.
+    This script initializes Windows 10/11 x64 and, then, installs initial software needed by employees at <insert company here>.
+.SYNOPSIS
+    This script initializes Windows 10/11 x64 and, then, installs initial software needed by employees at <insert company here>.
 #>
 
 #########################
 #       Functions       #
 #########################
 
-function InstallApps { #Check for Winget, Install Winget if it isn't installed, and then install apps from a list.
-    Write-Output "Checking for Winget..."
-    if (Get-Command winget) {
+function header($title) {
+    <#
+    .DESCRIPTION
+        Creates header for each function.
+    .SYNOPSIS
+        Creates header for each function.
+    #>
+    Write-Output "###################################`n# $title`n###################################"
+}
+
+function AAP($pkg) {
+    <#
+        .SYNOPSIS
+            Installs AppxPackage.
+        .DESCRIPTION
+            Installs AppxPackage.
+    #>
+    Add-AppxPackage $pkg -ErrorAction SilentlyContinue
+}
+
+function InstallPrereqs {
+    <#
+    .DESCRIPTION
+        Installs Winget Prerequisites from the .\prereqs directory.
+    .SYNOPSIS
+        Installs Winget Prerequisites from the .\prereqs directory.
+    #>
+
+    header("Installing Winget Prerequisites...")
+    AAP(.\packages\Microsoft.VCLibs.x64.14.00.Desktop.appx)
+    AAP(.\prereqs\Microsoft.UI.Xaml.2.7.appx)
+}
+
+function WingetCheck {
+    header("Checking for Winget...")
+    if (Get-Command -ErrorAction:SilentlyContinue winget) {
         Write-Output "Winget is installed.`nContinuing..."
     } else {
-        Write-Output "Winget doesn't exist.`nInstalling Winget...`nWinget is being downloaded..."
+        InstallPrereqs
+        Write-Output "Winget doesn't exist.`nWinget is being downloaded..."
         $ProgressPreference='Silent'
-        Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.3.2691/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile ( New-Item -Path .\packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -Force )
-        Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile .\packages\Microsoft.VCLibs.x64.14.00.Desktop.appx
+        Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.3.2691/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile .\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
         Write-Output "Installing winget..."
-        Add-AppxPackage .\packages\Microsoft.VCLibs.x64.14.00.Desktop.appx
-        Add-AppxPackage .\packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+        AAP(".\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle")
+        Write-Output "Cleaning up (Removing winget install file)..."
+        Remove-Item ".\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+        Write-Output "Refreshing Environment Variables..."
+        $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
     }
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
-    Set-ExecutionPolicy Unrestricted
-    Write-Output "Installing Applications..."
+}
+
+function InstallApps { #Check for Winget, Install Winget if it isn't installed, and then install apps from a list.
+    header("Installing Applications...")
     $Apps = @( #Define's application's names as appears in winget.
         "7zip.7zip",
         "Git.Git",
@@ -63,4 +101,5 @@ function InstallApps { #Check for Winget, Install Winget if it isn't installed, 
 #########################
 #    Call Functions     #
 #########################
+WingetCheck
 InstallApps
