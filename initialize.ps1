07ad4589d32ead56fb492890f0a12ce20fc555bf
Jonathan Rux <jonathan.e.rux@gmail.com>
2023-04-03 21:42:27 -0700

    Updated code to run winget locally.

--- initialize.ps1
+++ initialize.ps1
@@ -20,22 +20,6 @@
 #########################
 
 function InstallApps { #Check for Winget, Install Winget if it isn't installed, and then install apps from a list.
-    Write-Output "Checking for Winget..."
-    $Command = "winget" #try-catch using Get-Command to check if winget is present on system.
-    try {
-        if (Get-Command $Command) {
-            "$Command is installed.`nContinuing..."
-        }
-    } catch {
-        Write-Output "Winget doesn't exist.`nInstalling Winget..."
-        Set-ExecutionPolicy RemoteSigned #Allows the msixbundle installer to check signing with Microsoft's github.
-        $Winget = "https://github.com/microsoft/winget-cli/releases/download/v1.4.10173/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-        Write-Output "Winget is being downloaded..."
-        Invoke-WebRequest -Uri $Winget -OutFile winget-installer.msixbundle
-        Write-Output "Installing winget..."
-        .\winget-installer.msixbundle #Run the installer.
-    }
-
     Write-Output "Installing Applications"
     $Apps = @( #Define's application's names as appears in winget.
         @{name = "7zip.7zip"}
@@ -51,11 +35,12 @@ function InstallApps { #Check for Winget, Install Winget if it isn't installed, 
         @{name = "voidtools.Everything.Lite"}
     )
 
+    $Command = ".\winget-cli\winget.exe"
     foreach ($App in $Apps) {
         $listApp = winget list --exact -q $App.name
         if (![String]::Join("", $listApp).Contains($App.name)) {
             Write-Output "Installing: " $App.name
-            winget install -e -h --accept-source-agreements --accept-package-agreements --id $App.name
+            $Command install -e -h --accept-source-agreements --accept-package-agreements --id $App.name
         } else {
             Write-Output "Skipping:  $($App.name) is already installed; skipping."
         }
