2887d6286cd8955933c1e3622bd3e572c852c16a
Underscore <underscore@junior.lgbt>
2024-01-16 16:29:18 -0800

    Fixing prereq downloads.

--- initialize.ps1
+++ initialize.ps1
@@ -51,8 +51,10 @@ function InstallPrereqs {
     header("Installing Winget Prerequisites...")
     Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
     Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
-    Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-    Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
+    AAP("Microsoft.VCLibs.x64.14.00.Desktop.appx")
+    AAP("Microsoft.UI.Xaml.2.7.x64.appx")
+    Remove-Item Microsoft.VCLibs.x64.14.00.Desktop.appx
+    Remove-Item Microsoft.UI.Xaml.2.7.x64.appx
 }
 
 function WingetCheck {
