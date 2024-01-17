89a2ee705e689b8fdcd8111c46f3318a8bb4899c
Underscore <jonathan.e.rux@ruxunderscore.com>
2024-01-16 20:20:35 -0800

    Testing Merge
    ### Version 2.0
    
    - Removed prerequisites from repo.
    - Downloads and installs prerequisites.
    - Checks for the latest version of winget-cli and installs it.
    - Improved readability of code and added comments.
    - Changed how function parameters are handled.
    - Updated run directions and added screenshot of how the script looks when running.
    - Changed formatting of Outputs to terminal window.
    - Always looking for suggestions to improve the script!

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This PowerShell script initializes a Windows 10/11 x64 machine and installs initial software needed by employees at a company or for individual use. It uses the winget package manager to install applications listed in the `apps.json` file.
 
+![image](img/example.png)
+
 ## Requirements
 
 -   Windows 10/11 x64 machine
@@ -12,16 +14,17 @@ This PowerShell script initializes a Windows 10/11 x64 machine and installs init
 
 1. Clone this repository to your local machine.
 2. Open PowerShell as Administrator and navigate to the directory where the script is located.
-3. Run the script by entering the following command: `.\initialize.ps1`.
+3. Set Execution Policy to Bypass: `Set-ExecutionPolicy Bypass` **#Remember to never run scripts blindly.**
+4. Run the script by entering the following command: `.\initialize.ps1`.
 
 ## Functionality
 
 The script consists of the following functions:
 
-- `AAP($pkg)`: installs AppxPackage. 
-- `InstallPrereqs()`: installs winget prerequisites from the `.\prereqs` directory. 
-- `WingetCheck()`: checks for winget; if it doesn't exist, installs prerequisites and winget. 
-- `InstallApps()`: installs apps from JSON list.
+- `AAP`: installs AppxPackage. 
+- `InstallPrereqs`: installs winget prerequisites from the `.\prereqs` directory. 
+- `WingetCheck`: checks for winget; if it doesn't exist, installs prerequisites and winget. 
+- `InstallApps`: installs apps from JSON list. **#You may comment out InstallApps in the "Call Functions" section if you would only like the script to install Winget.**
 
 The script checks whether the winget package manager is installed on the machine. If it is not installed, the script installs the prerequisites for winget and then downloads and installs winget. After installing W=winget, the script installs the applications listed in the `.\apps.json` file using winget import.
 
--- initialize.ps1
+++ initialize.ps1
@@ -3,7 +3,7 @@
 #                                                       |  /  \    /  \__| ____    __| _/______  _  ________  #
 #       Author        : Jonathan Rux                    |  \   \/\/   /  |/    \  / __ |/  _ \ \/ \/ /  ___/  #
 #       Email         : jonathan.e.rux@underscore.com   |   \        /|  |   |  \/ /_/ (  <_> )     /\___ \   #
-#       Version       : 1.1                             |    \__/\  / |__|___|  /\____ |\____/ \/\_//____  >  #
+#       Version       : 2.0                             |    \__/\  / |__|___|  /\____ |\____/ \/\_//____  >  #
 #       OS Support    : Windows 10,11 x64               |         \/          \/      \/                 \/   #
 #                                                       |       Initialization Setup Script.                  #
 #                                                       |                                                     #
@@ -20,39 +20,82 @@
 #       Functions       #
 #########################
 
-function header($title) {
+
+# Function to create a header for each section
+function header {
+    param (
+        [string]$title
+    )
     <#
     .DESCRIPTION
         Creates header for each function.
     .SYNOPSIS
         Creates header for each function.
     #>
-    Write-Output "`n  $title`n=============================================`n"
+    Write-Output "$($title)`n$('='*64)`n"
 }
 
-function AAP($pkg) {
+# Function to install AppxPackage
+function AAP {
+    param (
+        [string]$pkg
+    )
     <#
     .SYNOPSIS
         Installs AppxPackage.
     .DESCRIPTION
         Installs AppxPackage.
     #>
-    Add-AppxPackage -ErrorAction:SilentlyContinue $pkg 
+    Write-Output "  Installing $($pkg)..."
+    Add-AppxPackage -ErrorAction:SilentlyContinue $pkg
+    Write-Output "  $($pkg) installed. Continuing...`n"
 }
 
+# Function to install prerequisites for Winget
 function InstallPrereqs {
     <#
     .DESCRIPTION
-        Installs Winget Prerequisites from the .\prereqs directory.
+        Downloads and Installs Winget Prerequisites.
     .SYNOPSIS
-        Installs Winget Prerequisites from the .\prereqs directory.
+        Downloads and Installs Winget Prerequisites.
     #>
 
-    header("Installing Winget Prerequisites...")
-    AAP(".\prereqs\Microsoft.VCLibs.x64.14.00.Desktop.appx")
-    AAP(".\prereqs\Microsoft.UI.Xaml.2.7.appx")
+    header -title "Installing winget-cli Prerequisites..."
+    Write-Output "  Downloading Microsoft.VCLibs.x64.14.00.Desktop.appx..."
+    Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+    Write-Output "  Downloading Microsoft.UI.Xaml.2.7.x64.appx...`n"
+    Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
+    AAP -pkg "Microsoft.VCLibs.x64.14.00.Desktop.appx"
+    AAP -pkg "Microsoft.UI.Xaml.2.7.x64.appx"
 }
 
+# Function to get the latest version of Winget from GitHub
+function Get-LatestGitHubRelease {
+    <#
+    .DESCRIPTION
+        Checks Github API for latest version of Winget.
+    .SYNOPSIS
+        Checks Github API for latest version of Winget.
+    #>
+    param (
+        [int]$assetIndex
+    )
+    
+    header -title "Checking GitHub API for the latest version of winget-cli..."
+    
+    try {
+        $response = Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+        $latestVersion = $response.tag_name
+        Write-Output "  Latest version:`t$($latestVersion)`n"
+        $assetUrl = $response.assets[$assetIndex].browser_download_url
+        Invoke-WebRequest -Uri $assetUrl -OutFile Microsoft.DesktopAppInstaller.msixbundle
+    } catch {
+        Write-Host "  Error: $_"
+    }
+}
+
+
+# Function to check for Winget; if not found, install prerequisites and Winget
 function WingetCheck {
     <#
     .SYNOPSIS
@@ -60,27 +103,21 @@ function WingetCheck {
     .DESCRIPTION
         Check for Winget; If doesn't exist, install prerequisites and winget.
     #>
-    header("Checking for Winget...")
-    if (Get-Command -ErrorAction:SilentlyContinue winget) {
-        Write-Output "Winget is installed.`nContinuing..."
-    } else {
-        Write-Output "Winget doesn't exist."
-        $ProgressPreference='Silent'
+    header -title "Checking for Winget..."
+    if (-not (Get-Command -ErrorAction SilentlyContinue winget)) {
+        Write-Host "  Winget not found. Installing prerequisites...`n"
         InstallPrereqs
-        Write-Output "Winget is being downloaded..."
-        $url="https://github.com/microsoft/winget-cli/releases/download/v1.3.2691/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-        $installer=".\Microsoft.DesktopAppInstaller.msixbundle"
-        Invoke-WebRequest -Uri $url -OutFile $installer
-        Write-Output "Installing winget..."
-        AAP($installer)
-        Write-Output "Cleaning up (Removing winget install file)..."
-        Remove-Item $installer
-        Write-Output "Refreshing Environment Variables..."
-        #Refresh environment variables. Thanks to weq in the PowerShell Discord for this.
-        $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+        Write-Host "  Downloading the latest winget-cli...`n"
+        Get-LatestGitHubRelease -assetIndex 2
+        Write-Output "  Installing winget-cli..."
+        AAP -pkg "Microsoft.DesktopAppInstaller.msixbundle"
+        Write-Output "  Refreshing Environment Variables...`n"
+        $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
     }
+    Write-Output "  Winget-cli is installed. Continuing...`n"
 }
 
+# Function to install applications from a JSON list
 function InstallApps {
     <#
     .SYNOPSIS
@@ -88,13 +125,19 @@ function InstallApps {
     .DESCRIPTION
         Install apps from json list.
     #>
-    header("Installing Applications...")
-    $Apps = ".\apps.json"
-    winget import -i $Apps --accept-package-agreements --accept-source-agreements
+    header -title "Installing Applications..."
+    $appsJson = ".\apps.json"
+    Write-Output "  Installing applications from $($appsJson)..."
+    winget import -i $appsJson --accept-package-agreements --accept-source-agreements
+    Write-Output "  Complete install of applications from $($appsJson)... Exiting..."
 }
 
 #########################
 #    Call Functions     #
 #########################
+
+# Check for Winget and install if not found
 WingetCheck
+
+# Install applications
 InstallApps
